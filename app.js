5eb463265607d0764bacc8d1891d901e8fed2d71
arpitfalcon <arpitfalcon1@gmail.com>
2020-07-01 17:59:24 +0530

    feat: Setting up the structure.

---
+++ app.js
@@ -0,0 +1,9 @@
+var budgetController = (function () {
+	// Some code here
+})();
+
+var UIController = (function () {
+	// Some code here
+})();
+
+var appController = (function (budgetCtrl, UICtrl) {})(budgetController, UIController);
