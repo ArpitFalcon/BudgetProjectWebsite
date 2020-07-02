d288441983a30c4f1df49b4da2ce8969d0ea1616
arpitfalcon <arpitfalcon1@gmail.com>
2020-07-02 09:20:19 +0530

    feat: Add the event listener.

--- app.js
+++ app.js
@@ -6,4 +6,14 @@ var UIController = (function () {
 	// Some code here
 })();
 
-var appController = (function (budgetCtrl, UICtrl) {})(budgetController, UIController);
+var appController = (function (budgetCtrl, UICtrl) {
+	document.querySelector('.add__btn').addEventListener('click', function () {
+		// ToDo : 1. Get the filled input data.
+		// ToDo : 2. Add the item to the budget controller
+		// ToDo : 3. Add the item to the UI
+		// ToDo : 4. Calculate the budget
+		// ToDo : 5. Display the budget on the UI
+	});
+
+	document.addEventListener('keypress', function (event) {});
+})(budgetController, UIController);
