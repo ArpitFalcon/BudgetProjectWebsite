3f2315b72254be405e2a4edaab8b96ded0e9acba
arpitfalcon <arpitfalcon1@gmail.com>
2020-07-02 09:43:54 +0530

    feat: Add function to take input from fields

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ var budgetController = (function () {
 })();
 
 var UIController = (function () {
-	// Some code here
+	return {
+		getinput: function () {
+			return {
+				type: document.querySelector('.add__type').value, // Output - "inc" / "exp".
+				description: document.querySelector('.add__description').value,
+				value: document.querySelector('.add__value').value,
+			};
+		},
+	};
 })();
 
 var appController = (function (budgetCtrl, UICtrl) {
-	document.querySelector('.add__btn').addEventListener('click', function () {
+	var ctrlAddItem = function () {
 		// ToDo : 1. Get the filled input data.
+		var input = UICtrl.getinput();
+
 		// ToDo : 2. Add the item to the budget controller
 		// ToDo : 3. Add the item to the UI
 		// ToDo : 4. Calculate the budget
 		// ToDo : 5. Display the budget on the UI
-	});
+	};
 
-	document.addEventListener('keypress', function (event) {});
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function (event) {
+		if (event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
 })(budgetController, UIController);
